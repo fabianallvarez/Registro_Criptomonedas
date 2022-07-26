21fd3dc584b0a3a8bd2262afee3a0dbd88f07898
fabianallvarez <fabianallvarez@gmail.com>
2022-07-26 16:15:11 +0200

    Ft: Agregando en models

--- balance/models.py
+++ balance/models.py
@@ -1,5 +1,28 @@
+import sqlite3
+
 class DBManager:
     def __init__(self, ruta):
         self.ruta = ruta
 
+    def consultaSQL(self, consulta):
+        conexion = sqlite3.connect(self.ruta)
+        cursor = conexion.cursor()
+        cursor.execute(consulta)
+        self.movimientos = []
+        nombres_columnas = []
         
+        for desc_columna in cursor.description:
+            nombres_columnas.append(desc_columna[0])
+
+        datos = cursor.fetchall()
+        for dato in datos:
+            movimiento = {}
+            indice = 0
+            for nombre in nombres_columnas:
+                movimiento[nombre] = dato[indice]
+                indice += 1
+            self.movimientos.append(movimiento)
+
+        conexion.close()
+
+        return self.movimientos
